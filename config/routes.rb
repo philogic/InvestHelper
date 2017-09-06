e94cda02c51903409aa43f576ca0d3cb07df9362
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2017-09-06 05:09:07 +0200

    Create stocks controller and search_stocks template.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'stocks/search'
+
   get 'my_stocks', to: 'users#my_stocks'
+  get 'search_stocks', to: 'stocks#search'
 
   devise_for :users
   get 'welcome/index'
